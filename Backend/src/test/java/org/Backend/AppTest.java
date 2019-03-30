d06b759e132eff029e37cb4ba7780f6a633442cf
Oliver <zedasgm@gmail.com>
2019-03-30 02:47:50 +0100

    cleanup after h2 integration

--- Backend/src/test/java/org/Backend/AppTest.java
+++ Backend/src/test/java/org/Backend/AppTest.java
@@ -11,20 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Config.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = Config.class)
 public class AppTest {
-
-	@Autowired
-	private H2Test testMeH2;
-	
-	
-	@Test
-	public void testH2() {
-	assertNotNull(testMeH2);
-	testMeH2.callMeH2();
-		
-	}
 
 }
 
