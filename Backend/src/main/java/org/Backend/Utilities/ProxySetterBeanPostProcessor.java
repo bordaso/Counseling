46e64ce72d11e07fcf9a4b436df01d5fee52ba8e
Oliver <zedasgm@gmail.com>
2019-04-26 02:05:59 +0200

    updated ProxySetterBeanPostProcessor

--- Backend/src/main/java/org/Backend/Utilities/ProxySetterBeanPostProcessor.java
+++ Backend/src/main/java/org/Backend/Utilities/ProxySetterBeanPostProcessor.java
@@ -1,6 +1,9 @@
 package org.Backend.Utilities;
 
+import org.Backend.DAOs.BookingDetailsDao;
+import org.Backend.DAOs.BookingsDao;
 import org.Backend.DAOs.EmployeeDao;
+import org.Backend.DAOs.NotificationSetupDao;
 import org.Backend.DAOs.PatientDao;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -15,10 +18,24 @@ public class ProxySetterBeanPostProcessor implements BeanPostProcessor {
 		if (bean instanceof EmployeeDao) {
 			EmployeeDao proxyBean = (EmployeeDao) bean;
 			proxyBean.setMyProxy(proxyBean);
-        }else if (bean instanceof PatientDao) {
+        }else 
+        	if (bean instanceof PatientDao) {
         	PatientDao proxyBean = (PatientDao) bean;
 			proxyBean.setMyProxy(proxyBean);
+        }else 
+        	if (bean instanceof BookingsDao) {
+        	BookingsDao proxyBean = (BookingsDao) bean;
+			proxyBean.setMyProxy(proxyBean);
+        }else 
+        	if (bean instanceof BookingDetailsDao) {
+        	BookingDetailsDao proxyBean = (BookingDetailsDao) bean;
+			proxyBean.setMyProxy(proxyBean);
+        }else 
+        	if (bean instanceof NotificationSetupDao) {
+        	NotificationSetupDao proxyBean = (NotificationSetupDao) bean;
+			proxyBean.setMyProxy(proxyBean);
         }
+		
 		return bean;
 	}
 
